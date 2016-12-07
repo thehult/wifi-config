f027c973bf63e81b7a1e7ef2fc32c9c271e8aa0b
Hult <hultqvist.tobias@gmail.com>
2016-12-07 21:32:04 +0100

    Started webapp

---
+++ web/app.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var https = require('https');
+var app = require('express')();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var config = require('./config.js');
+
+mongoose.connect(config.mongoose);
+
+app.use(bodyParser( bodyParser.json()));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function(req, res) {
+  res.send('HELLO SIOS');
+});
+
+app.post('/greenhouse/update', function(req, res) {
+  fs.appendFile('log.txt', req.body.test, function(err) {
+    if(err) throw err;
+    console.log('Logged: ' + req.body.test);
+  });
+});
+
+https.createServer(config.ssl, app).listen(3000, function() {
+  console.log("Started at port 3000");
+});

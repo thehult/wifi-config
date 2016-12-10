df663c1093a3f45b8a5b1ecef236c4b39cf9e748
Hult <tobhu543@student.liu.se>
2016-12-10 23:17:12 +0100

    Configurated static IP

--- rpi/wifi-config/index.js
+++ rpi/wifi-config/index.js
@@ -1,5 +1,6 @@
 var ifconfig = require('wireless-tools/ifconfig');
 var hostapd = require('wireless-tools/hostapd');
+var udhcpd = require('wireless-tools/udhcpd');
 var http = require('http');
 
 
@@ -13,12 +14,39 @@ module.exports = function(opts, cb) {
         res.end("IT WOOOOORKS!")
     }
 
+
     function startAccessPoint() {
         _log("Starting Access Point");
         options.accessPoint.interface = options.interface;
-        hostapd.enable(options.accessPoint, function(err) {
-            if (err) return console.log(err);
-            accessPoint_started()
+
+        ifconfig.down(options.interface, function(err) {
+            if (err) return callback(err);
+            var up_options = {
+                interface: 'wlan0',
+                ipv4_address: '192.168.10.1',
+                ipv4_broadcast: '192.168.10.255',
+                ipv4_subnet_mask: '255.255.255.0'
+            };
+            ifconfig.up(up_options, function(err) {
+                if (err) return callback(err);
+                var dhcp_options = {
+                    interface: options.interface,
+                    start: '192.168.10.100',
+                    end: '192.168.10.200',
+                    option: {
+                        router: '192.168.10.1',
+                        subnet: '255.255.255.0',
+                        dns: [ '4.4.4.4', '8.8.8.8' ]
+                    }
+                };
+                udhcpd.enable(dhcp_options, function(err) {
+                    if (err) return callback(err);
+                    hostapd.enable(options.accessPoint, function(err) {
+                        if (err) return callback(err);
+                        accessPoint_started()
+                    });
+                });
+            });
         });
     }
 

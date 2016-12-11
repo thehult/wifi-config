8ded688da2f57a8f6b15ed88baf696d21977cc74
Hult <tobhu543@student.liu.se>
2016-12-11 11:31:10 +0100

    Added gitignore and a wait for ifconfig_up

--- rpi/wifi-config/index.js
+++ rpi/wifi-config/index.js
@@ -4,6 +4,7 @@ var udhcpd = require('wireless-tools/udhcpd');
 var iwlist = require('wireless-tools/iwlist')
 
 var series = require('async/series');
+var until = require('async/until');
 
 var http = require('http');
 
@@ -19,9 +20,30 @@ module.exports = function(opts, cb) {
         res.end("YO!");
     }
 
+    function _ifconfig_wait_for_up(cb) {
+        var isUp = false;
+        until(function() {
+            return isUp;
+        }, function(_cb) {
+            ifconfig.status(options.interface, function(err, status) {
+                if(err) return _cb(err);
+                if(typeof status === 'undefined')
+                    return _cb(null);
+                if(typeof status.ipv4_address !== 'string')
+                    return _cb(null);
+                isUp = true;
+                return _cb(null);
+            });
+        }, function(err, results) {
+            if(err) return cb(err);
+            cb(null);
+        });
+
+    }
+
     function _iwlist_scan(cb) {
         iwlist.scan({ iface: options.interface, show_hidden: true}, function(err, networks) {
-            if(err) cb(err);
+            if(err) return cb(err);
             _log("scanned wifi");
             cb(null, networks);
         });
@@ -38,7 +60,7 @@ module.exports = function(opts, cb) {
     function _ifconfig_up(cb) {
         options.iface.interface = options.interface;
         ifconfig.up(options.iface, function(err) {
-            if(err) cb(err);
+            if(err) return cb(err);
             _log(options.interface + " up");
             cb(null);
         });
@@ -46,7 +68,7 @@ module.exports = function(opts, cb) {
 
     function _udhcpd_disable(cb) {
         udhcpd.disable(options.interface, function(err) {
-            if(err) cb(err);
+            if(err) return cb(err);
             _log("udhcpd disabled");
             cb(null);
         });
@@ -55,7 +77,7 @@ module.exports = function(opts, cb) {
     function _udhcpd_enable(cb) {
         options.dhcp.interface = options.interface;
         udhcpd.enable(options.dhcp, function(err) {
-            if(err) cb(err);
+            if(err) return cb(err);
             _log("udhcpd enabled");
             cb(null);
         });
@@ -63,7 +85,7 @@ module.exports = function(opts, cb) {
 
     function _hostapd_disable(cb) {
         hostapd.disable(options.interface, function(err) {
-            if(err) cb(err);
+            if(err) return cb(err);
             _log("hostapd disabled");
             cb(null);
         });
@@ -72,7 +94,7 @@ module.exports = function(opts, cb) {
     function _hostapd_enable(cb) {
         options.accessPoint.interface = options.interface;
         hostapd.enable(options.accessPoint, function(err) {
-            if(err) cb(err);
+            if(err) return cb(err);
             _log("hostapd enabled");
             cb(null);
         });
@@ -87,6 +109,7 @@ module.exports = function(opts, cb) {
             _udhcpd_disable,
             _ifconfig_down,
             _ifconfig_up,
+            _ifconfig_wait_for_up,
             _iwlist_scan,
             _udhcpd_enable,
             _hostapd_enable
